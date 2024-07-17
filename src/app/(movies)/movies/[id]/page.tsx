f793fd5837feb5e7c25ad7ab30c239b5bd18765f
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-07-17 14:12:03 +0900

    Fetching(Parallel Requests)

--- src/app/(movies)/movies/[id]/page.tsx
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,25 @@
-export default function MovieDetail({
+import { API_URL } from '@/api';
+
+async function getMovie(id: string) {
+  console.log(`Fetching movie ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+async function getVideo(id: string) {
+  console.log(`Fetching video ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  console.log('start fetch');
+  const [movie, video] = await Promise.all([getMovie(id), getVideo(id)]);
+  console.log('end fetch');
+  return <h1>{movie.title}</h1>;
 }
