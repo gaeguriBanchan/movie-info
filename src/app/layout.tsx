1e87be412d19bfaadc464fac43665757cf051796
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-07-17 13:48:29 +0900

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Navigation from '@/components/navigation';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Next Movies',
+    default: 'Next Movies',
+  },
+  description: 'The best movies',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navigation />
+        {children}
+        &copy; Next JS is great!
+      </body>
+    </html>
+  );
+}
