7202a9d75d5ac8c15f029e3e35dd9b397a1c8806
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-07-17 14:42:56 +0900

    Error Handling

--- src/components/movie-videos.tsx
+++ src/components/movie-videos.tsx
@@ -3,8 +3,9 @@ import { API_URL } from '@/api';
 async function getVideo(id: string) {
   console.log(`Fetching video ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  throw new Error('something broke...');
+  // const response = await fetch(`${API_URL}/${id}/videos`);
+  // return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
