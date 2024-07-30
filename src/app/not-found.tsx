34069a4c5a9b667016c9882f5ec46069b67b5a3e
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-07-30 14:50:05 +0900

    NotFound CSS

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,14 @@
 import { Metadata } from 'next';
+import styles from '@/styles/not-found.module.css';
 
 export const metadata: Metadata = {
   title: 'Not Found',
 };
 
 export default function NotFound() {
-  return <h1>Not Found!</h1>;
+  return (
+    <div className={styles.main}>
+      <h1>Not Found</h1>
+    </div>
+  );
 }
